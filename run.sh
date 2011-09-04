bf9465f8863a8cccf42be465f97a523b28a3871e
Andrew Etches <andy@ignignokt.co.uk>
2011-09-04 02:25:48 +0100

    Changed run script to rely on M2_REPO

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-java -Xmn5M -Xms10M -Xmx10M -XX:MaxPermSize=16m -cp "$HOME/.m2/repository/pircbot/pircbot/1.5.0/pircbot-1.5.0.jar:$HOME/.m2/repository/Markov/Markov/0.1.0/Markov-0.1.0.jar:$HOME/.m2/repository/MarkovBot/MarkovBot/0.1.0/MarkovBot-0.1.0.jar" uk.co.ignignokt.markovbot.Main &
+if [ ! -n "${M2_REPO}" ]; then
+    echo "M2_REPO not set";
+    exit;
+fi
+
+if [ "$M2_REPO" == "" ]; then
+    echo "M2_REPO blank";
+    exit;
+fi
+
+java -Xmn5M -Xms10M -Xmx10M -XX:MaxPermSize=16m -cp "$M2_REPO/pircbot/pircbot/1.5.0/pircbot-1.5.0.jar:$M2_REPO/Markov/Markov/0.1.0/Markov-0.1.0.jar:$M2_REPO/MarkovBot/MarkovBot/0.1.0/MarkovBot-0.1.0.jar" uk.co.ignignokt.markovbot.Main &
