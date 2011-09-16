a51ba0a07fe77947cc48c6ecb70522c05f52f768
Andrew Etches <andy@ignignokt.co.uk>
2011-09-16 12:36:11 +0100

    Added a throttle timer of 30s

--- src/main/java/uk/co/ignignokt/markovbot/bot/MarkovBot.java
+++ src/main/java/uk/co/ignignokt/markovbot/bot/MarkovBot.java
@@ -9,8 +9,12 @@ import uk.co.ignignokt.markovbot.Backup;
 
 public class MarkovBot extends PircBot {
 
-        Chain chain;
-        Backup backup;
+        private Chain chain;
+        private Backup backup;
+        
+        private static final long THROTTLETIME = 30000;
+        
+        private long throttle;
 
         public void setNick(String nick){
                 this.setName(nick);
@@ -19,16 +23,26 @@ public class MarkovBot extends PircBot {
         public MarkovBot(Chain chain, Backup backup) {
                 this.chain = chain;
                 this.backup = backup;
+                this.throttle = System.currentTimeMillis();
+                
+                setVersion("Markov Chain Bot v0.1");
         }
 
         @Override
         public void onMessage(String channel, String sender, String login, String hostname, String message) {
                 if (message.matches("(!line)|!line .*")) {
+                        long time = System.currentTimeMillis();
+                        if(time - this.throttle < THROTTLETIME){
+                                sendMessage(sender, "Sorry, please wait " + (THROTTLETIME - (time - this.throttle))/1000 + "s before trying again");
+                                return;
+                        }
+
                         String x = chain.getSentence();
                         while (x.split(" ").length < 6) {
                                 x = chain.getSentence();
                         }
                         sendMessage(channel, x);
+                        this.throttle = System.currentTimeMillis();
                 } else {
                         chain.addSentence(message);
                         try {
